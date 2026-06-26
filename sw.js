476b47081f9cb983d2b1d64e824d9d9cd9a28c6d
Rub19 <rub19.mailpro@gmail.com>
2026-06-26 11:29:53 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const ETHONE_VERSION = "2026-06-26-production-v3";
+const ETHONE_VERSION = "2026-06-26-production-v7-emergency-safe";
 const ETHONE_CACHE = `ethone-${ETHONE_VERSION}`;
 const ETHONE_OFFLINE_URL = "./index.html";
 const ETHONE_CORE_ASSETS = [

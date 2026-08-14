0de779972bc9502927fca2f1a7d32702faee51fc
Rub19 <rub19.mailpro@gmail.com>
2026-08-14 13:23:27 +0200

    Migration Next.js : bump PWA cache v346 pour forcer rechargement dashboard topbar/status bar responsive

--- ethone-next/public/sw.js
+++ ethone-next/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ethone-next-v345";
+const CACHE_NAME = "ethone-next-v346";
 const PRECACHE = ["/", "/login/", "/offline.html"];
 const STATIC_EXTENSIONS = [".js", ".css", ".png", ".jpg", ".jpeg", ".webp", ".svg", ".woff", ".woff2", ".ico"];
 

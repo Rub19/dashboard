cd40e402d4798166d0f66f907e6ed2a313ee36f0
Rub19 <rub19.mailpro@gmail.com>
2026-08-19 20:00:34 +0200

    Migration Next.js : cache Service Worker v351

--- ethone-next/public/sw.js
+++ ethone-next/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ethone-next-v350";
+const CACHE_NAME = "ethone-next-v351";
 const PRECACHE = ["/", "/login/", "/offline.html"];
 const STATIC_EXTENSIONS = [".js", ".css", ".png", ".jpg", ".jpeg", ".webp", ".svg", ".woff", ".woff2", ".ico"];
 

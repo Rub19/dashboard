39bfa5f692c9938317f160d09613c3c0c88e678b
Rub19 <rub19.mailpro@gmail.com>
2026-08-19 19:06:44 +0200

    Migration Next.js : cache SW v350 + suppression debug sync

--- ethone-next/public/sw.js
+++ ethone-next/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ethone-next-v349";
+const CACHE_NAME = "ethone-next-v350";
 const PRECACHE = ["/", "/login/", "/offline.html"];
 const STATIC_EXTENSIONS = [".js", ".css", ".png", ".jpg", ".jpeg", ".webp", ".svg", ".woff", ".woff2", ".ico"];
 

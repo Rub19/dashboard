55d1c7496a1937782b0c364baa44b6bcb861c96d
Rub19 <rub19.mailpro@gmail.com>
2026-08-26 20:09:26 +0200

    Migration Next.js : mise a jour URL Stripe de soutien

--- ethone-next/lib/env.ts
+++ ethone-next/lib/env.ts
@@ -1,3 +1,3 @@
 export const STRIPE_DONATION_URL =
   process.env.NEXT_PUBLIC_STRIPE_DONATION_URL ||
-  "https://donate.stripe.com/test_fZu5kD8923u73gn3Bv4Ni00";
+  "https://buy.stripe.com/fZu5kD8923u73gn3Bv4Ni00";
